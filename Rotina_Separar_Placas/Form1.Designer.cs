a5aafbb8d7d7fd3277a4e4ccb3ba7c488a0634bb
mafioletti98 <matemafioletti@gmail.com>
2024-10-04 12:28:57 -0300

    Adicionando arquivos do projeto e configurando .gitignore

---
+++ Rotina_Separar_Placas/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Rotina_Separar_Placas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnDirDestino = new System.Windows.Forms.Button();
+            this.btnDirMaster = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelecionarArquivo = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.pnlTop);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1005, 690);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 39);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowCellToolTips = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1003, 649);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.MediumBlue;
+            this.pnlTop.Controls.Add(this.btnSelecionarArquivo);
+            this.pnlTop.Controls.Add(this.btnExecutar);
+            this.pnlTop.Controls.Add(this.btnDirDestino);
+            this.pnlTop.Controls.Add(this.btnDirMaster);
+            this.pnlTop.Controls.Add(this.label1);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(1003, 39);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(941, 8);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(51, 23);
+            this.btnExecutar.TabIndex = 3;
+            this.btnExecutar.Text = "EXECUTAR";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnDirDestino
+            // 
+            this.btnDirDestino.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnDirDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDirDestino.Location = new System.Drawing.Point(758, 8);
+            this.btnDirDestino.Name = "btnDirDestino";
+            this.btnDirDestino.Size = new System.Drawing.Size(177, 23);
+            this.btnDirDestino.TabIndex = 2;
+            this.btnDirDestino.Text = "Selecionar Dir DESTINO";
+            this.btnDirDestino.UseVisualStyleBackColor = true;
+            this.btnDirDestino.Click += new System.EventHandler(this.btnDirDestino_Click);
+            // 
+            // btnDirMaster
+            // 
+            this.btnDirMaster.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnDirMaster.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDirMaster.Location = new System.Drawing.Point(599, 8);
+            this.btnDirMaster.Name = "btnDirMaster";
+            this.btnDirMaster.Size = new System.Drawing.Size(153, 23);
+            this.btnDirMaster.TabIndex = 1;
+            this.btnDirMaster.Text = "Selecionar Dir Master";
+            this.btnDirMaster.UseVisualStyleBackColor = true;
+            this.btnDirMaster.Click += new System.EventHandler(this.btnDirMaster_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(520, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rotina ler planilha -> localizar foto -> copiar para o destino com o diretorio";
+            // 
+            // btnSelecionarArquivo
+            // 
+            this.btnSelecionarArquivo.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSelecionarArquivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelecionarArquivo.Location = new System.Drawing.Point(440, 8);
+            this.btnSelecionarArquivo.Name = "btnSelecionarArquivo";
+            this.btnSelecionarArquivo.Size = new System.Drawing.Size(153, 23);
+            this.btnSelecionarArquivo.TabIndex = 4;
+            this.btnSelecionarArquivo.Text = "Selecionar Arquivo XLS";
+            this.btnSelecionarArquivo.UseVisualStyleBackColor = true;
+            this.btnSelecionarArquivo.Click += new System.EventHandler(this.btnSelecionarArquivo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 690);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rotina Separa Foto";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDirMaster;
+        private System.Windows.Forms.Button btnDirDestino;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSelecionarArquivo;
+    }
+}
---
+++ Rotina_Separar_Placas/Form1.cs
@@ -0,0 +1,289 @@
+﻿using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using NPOI.HSSF.UserModel;
+using System.Data;
+
+
+
+
+namespace Rotina_Separar_Placas
+{
+    public partial class Form1 : Form
+    {
+
+        public string selectedExcelFilePath1;
+        public string selectedImageDirectory1;
+        public string selectedDestinationDirectory1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDirMaster_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedImageDirectory1 = folderBrowserDialog.SelectedPath;
+                    btnDirMaster.Text = selectedImageDirectory1;
+
+                    MessageBox.Show("Diretório selecionado: " + selectedImageDirectory1);
+                }
+            }
+
+        }
+
+        private void btnDirDestino_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    selectedDestinationDirectory1 = fbd.SelectedPath;
+                    btnDirDestino.Text = selectedDestinationDirectory1;
+                }
+            }
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                string cell = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                string caminhoCompleto = Path.Combine(selectedImageDirectory1, cell);
+                if (File.Exists(caminhoCompleto))
+                {
+                    string caminhoCompletoDestino = Path.Combine(selectedDestinationDirectory1, cell);
+                    if (!File.Exists(caminhoCompletoDestino))
+                    {
+                        string criarDiretorio = Path.GetDirectoryName(cell);
+                        string caminhoCompleto2 = Path.Combine(selectedDestinationDirectory1, criarDiretorio);
+                        Directory.CreateDirectory(caminhoCompleto2);
+                    }
+                    string caminhoCompleto3 = Path.Combine(selectedDestinationDirectory1, cell);
+                    File.Copy(caminhoCompleto, caminhoCompleto3);
+                }
+            }
+            MessageBox.Show("Feito!");
+        }
+
+        string arquivoXls;
+
+        private void btnSelecionarArquivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                arquivoXls = ofd.FileName;
+                MessageBox.Show("Arquivo Xls Selecionado");
+                PreencherDataGridComExcel(arquivoXls, dataGridView1);
+                dataGridView1.ClearSelection();
+            }
+        }
+
+        public void PreencherDataGridComExcel(string filePath, DataGridView dataGridView)
+        {
+            try
+            {
+                IWorkbook workbook;
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    if (Path.GetExtension(filePath).ToLower() == ".xls")
+                    {
+                        workbook = new HSSFWorkbook(fileStream); // Para arquivos .xls
+                    }
+                    else if (Path.GetExtension(filePath).ToLower() == ".xlsx")
+                    {
+                        workbook = new XSSFWorkbook(fileStream); // Para arquivos .xlsx
+                    }
+                    else
+                    {
+                        throw new Exception("Formato de arquivo não suportado");
+                    }
+                }
+
+                ISheet sheet = workbook.GetSheetAt(0); // Lê a primeira aba da planilha
+                DataTable dt = new DataTable();
+
+                // Ler as colunas (cabeçalho)
+                IRow headerRow = sheet.GetRow(0);
+                int cellCount = headerRow.LastCellNum;
+                for (int i = 0; i < cellCount; i++)
+                {
+                    dt.Columns.Add(headerRow.GetCell(i).ToString());
+                }
+
+                // Ler as linhas (dados)
+                for (int i = 1; i <= sheet.LastRowNum; i++)
+                {
+                    IRow row = sheet.GetRow(i);
+                    DataRow dataRow = dt.NewRow();
+
+                    for (int j = 0; j < cellCount; j++)
+                    {
+                        dataRow[j] = row.GetCell(j)?.ToString() ?? string.Empty;
+                    }
+
+                    dt.Rows.Add(dataRow);
+                }
+
+                // Vincular o DataTable ao DataGridView
+                dataGridView.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao ler o arquivo Excel: " + ex.Message);
+            }
+        }
+    }
+}
+
+
+//using NPOI.XSSF.UserModel;
+//using NPOI.SS.UserModel;
+//using System;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Windows.Forms;
+//using NPOI.HSSF.UserModel;
+//using System.Data;
+
+//namespace Rotina_Separar_Placas
+//{
+//    public partial class Form1 : Form
+//    {
+//        public string selectedExcelFilePath1;
+//        public string selectedImageDirectory1;
+//        public string selectedDestinationDirectory1;
+
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void btnDirMaster_Click(object sender, EventArgs e)
+//        {
+//            selectedImageDirectory1 = SelectFolder("Diretório selecionado:");
+//            btnDirMaster.Text = selectedImageDirectory1;
+//        }
+
+//        private void btnDirDestino_Click(object sender, EventArgs e)
+//        {
+//            selectedDestinationDirectory1 = SelectFolder();
+//            btnDirDestino.Text = selectedDestinationDirectory1;
+//        }
+
+//        private void btnExecutar_Click(object sender, EventArgs e)
+//        {
+//            foreach (DataGridViewRow row in dataGridView1.Rows)
+//            {
+//                string cell = row.Cells[1].Value.ToString();
+//                string sourcePath = Path.Combine(selectedImageDirectory1, cell);
+//                string destinationPath = Path.Combine(selectedDestinationDirectory1, cell);
+
+//                if (File.Exists(sourcePath) && !File.Exists(destinationPath))
+//                {
+//                    string destinationDirectory = Path.GetDirectoryName(destinationPath);
+//                    Directory.CreateDirectory(destinationDirectory);
+//                    File.Copy(sourcePath, destinationPath);
+//                }
+//            }
+//            MessageBox.Show("Feito!");
+//        }
+
+//        string arquivoXls;
+
+//        private void btnSelecionarArquivo_Click(object sender, EventArgs e)
+//        {
+//            arquivoXls = SelectFile("Arquivo Xls Selecionado", "Excel Files|*.xls;*.xlsx");
+//            if (arquivoXls != null)
+//            {
+//                PreencherDataGridComExcel(arquivoXls, dataGridView1);
+//                dataGridView1.ClearSelection();
+//            }
+//        }
+
+//        private string SelectFolder(string message = "")
+//        {
+//            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+//            {
+//                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+//                {
+//                    if (!string.IsNullOrEmpty(message))
+//                    {
+//                        MessageBox.Show(message + folderBrowserDialog.SelectedPath);
+//                    }
+//                    return folderBrowserDialog.SelectedPath;
+//                }
+//            }
+//            return null;
+//        }
+
+//        private string SelectFile(string successMessage, string filter = "")
+//        {
+//            using (OpenFileDialog ofd = new OpenFileDialog())
+//            {
+//                if (!string.IsNullOrEmpty(filter))
+//                {
+//                    ofd.Filter = filter;
+//                }
+
+//                if (ofd.ShowDialog() == DialogResult.OK)
+//                {
+//                    MessageBox.Show(successMessage);
+//                    return ofd.FileName;
+//                }
+//            }
+//            return null;
+//        }
+
+//        public void PreencherDataGridComExcel(string filePath, DataGridView dataGridView)
+//        {
+//            try
+//            {
+//                IWorkbook workbook = WorkbookFactory.Create(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+//                ISheet sheet = workbook.GetSheetAt(0);
+//                DataTable dt = new DataTable();
+
+//                // Ler as colunas (cabeçalho)
+//                IRow headerRow = sheet.GetRow(0);
+//                for (int i = 0; i < headerRow.LastCellNum; i++)
+//                {
+//                    dt.Columns.Add(headerRow.GetCell(i).ToString());
+//                }
+
+//                // Ler as linhas (dados)
+//                for (int i = 1; i <= sheet.LastRowNum; i++)
+//                {
+//                    IRow row = sheet.GetRow(i);
+//                    DataRow dataRow = dt.NewRow();
+
+//                    for (int j = 0; j < headerRow.LastCellNum; j++)
+//                    {
+//                        dataRow[j] = row.GetCell(j)?.ToString() ?? string.Empty;
+//                    }
+
+//                    dt.Rows.Add(dataRow);
+//                }
+
+//                dataGridView.DataSource = dt;
+//            }
+//            catch (Exception ex)
+//            {
+//                MessageBox.Show("Erro ao ler o arquivo Excel: " + ex.Message);
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
